c646015f1f1d7443f371995de3d2eac3b058d851
vishnu narayanan <vishnunarayanan2044@gmail.com>
2024-03-22 12:00:00 +0530

    static file error fixing

--- BookStall/settings.py
+++ BookStall/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 
     'cloudinary_storage',
     'django.contrib.staticfiles',
